620b14ac7bcc01d22381d1e9a42f0848dca10d1b
Yugo <satoyugo1022@gmail.com>
2023-08-02 16:05:59 +0900

    add

---
+++ src/pages/news.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import style from '../styles/test.module.scss'
+
+const news = () => {
+  return (
+    <div className='text-5xl underline'>
+      News
+    <p className={style.test}>Test</p>
+    </div>
+  )
+}
+
+export default news ;
